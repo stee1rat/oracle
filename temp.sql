7789b87834bf5e4f8706e9ad26a0fef40e0661c6
Абоймов Василий Анатольевич <vaaboimov@cplsb.ru>
2017-09-18 11:25:54 +0300

    new files

--- temp.sql
+++ temp.sql
@@ -5,11 +5,9 @@ SELECT username,
        round(blocks*8192/1024/1024/1024,2) 
   FROM v$tempseg_usage 
  ORDER BY 4 desc;
-/
 
 -- temp usage
 SELECT sysdate,
        (SELECT round(sum(blocks)*8192/1024/1024/1024,2) used_total FROM v$tempseg_usage) used,
        (SELECT round(sum(bytes)/1024/1024/1024 FROM dba_temp_files, 2) allocated
   FROM dual;
-/
