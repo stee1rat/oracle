a9b4e5f083258bd454016f4ffd44191957049264
Vasiliy Aboymov <v.aboymov@tinkoff.ru>
2023-11-10 15:11:17 +0300

    small fix

--- sql_stat_history.sql
+++ sql_stat_history.sql
@@ -1,13 +1,18 @@
-select h.begin_interval_time,
-       s.sql_id,
-       s.plan_hash_value,
-       s.executions_delta,
-       round(s.buffer_gets_delta/s.executions_delta,2) buffer_gets,
-       round(s.elapsed_time_delta/decode(s.executions_delta,0,1,s.executions_delta)/1e6,5) elapsed_seconds
-  from dba_hist_sqlstat s,
-       dba_hist_snapshot h
- where sql_id = 'adc0nxpxm0ghs' 
-   and h.dbid = 3647405474
-   and s.snap_id = h.snap_id
-   and s.executions_delta > 0
- order by s.snap_id;  
+select h.begin_interval_time, s.sql_id, s.plan_hash_value, s.sql_profile, s.executions_total,
+       trunc(decode(executions_total, 0, 0, rows_processed_total/executions_total)) rows_avg,
+       trunc(decode(executions_total, 0, 0, fetches_total/executions_total)) fetches_avg,
+       trunc(decode(executions_total, 0, 0, disk_reads_total/executions_total)) disk_reads_avg,
+       trunc(decode(executions_total, 0, 0, buffer_gets_total/executions_total)) buffer_gets_avg,
+       trunc(decode(executions_total, 0, 0, cpu_time_total/executions_total)) cpu_time_avg,
+       trunc(decode(executions_total, 0, 0, elapsed_time_total/executions_total)) elapsed_time_avg,
+       trunc(decode(executions_total, 0, 0, parse_calls_total/executions_total)) parse_calls_avg,
+       trunc(decode(executions_total, 0, 0, iowait_total/executions_total)) iowait_time_avg,
+       trunc(decode(executions_total, 0, 0, clwait_total/executions_total)) clwait_time_avg,
+       trunc(decode(executions_total, 0, 0, apwait_total/executions_total)) apwait_time_avg,
+       trunc(decode(executions_total, 0, 0, ccwait_total/executions_total)) ccwait_time_avg,
+       trunc(decode(executions_total, 0, 0, plsexec_time_total/executions_total)) plsexec_time_avg,
+       trunc(decode(executions_total, 0, 0, javexec_time_total/executions_total)) javexec_time_avg
+ from dba_hist_sqlstat s, dba_hist_snapshot h
+where s.sql_id = '9m3x403xtzzq1'
+  and h.snap_id = s.snap_id
+order by s.sql_id, h.begin_interval_time desc;
